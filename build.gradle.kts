6a26fbbc9fe53d072c0df9cf0939f91471fff902
Thomas Mohme <2261413+tmohme@users.noreply.github.com>
2019-08-17 21:53:22 +0200

    upgrade to Gradle 5.6; automatic minification on "optimize=true"

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ elm {
   targetModuleName.set("main.js")
   debug.set(true)
   optimize.set(false)
+  executionDir.set(projectDir.canonicalPath)
+  testExecutionDir.set(projectDir.canonicalPath)
 }
 
 tasks {
@@ -27,4 +29,18 @@ tasks {
 
   assemble.get().dependsOn(elmMake, copy)
   check.get().dependsOn(elmTest)
+
+  val minify = register("minify", Exec::class.java) {
+    group = "build"
+    description = "minification of produced javascript code"
+    dependsOn(elmMake)
+
+    this.onlyIf {
+      project.elm.optimize.get()
+    }
+
+    executable = "bash"
+    args("-c", "uglifyjs build/elm/main.js --compress 'pure_funcs=\"F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9\",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=build/elm/main.min.js")
+  }
+  assemble.get().dependsOn(minify.get())
 }
